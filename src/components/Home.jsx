700a6c8a35bc0338b5a6b3f4012a4524b193286e
Dipanshu Kumar <dipanshukumar344@gmail.com>
2023-06-14 23:27:49 +0530

    VideoHub

---
+++ src/components/Home.jsx
@@ -0,0 +1,101 @@
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
+
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%,-50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
+};
+
+const Home = () => {
+  return (
+    <Box>
+      <MyCarousel />
+
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
+        >
+          Services
+        </Heading>
+
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+            Video Hub is a dynamic online platform that serves as a centralized hub for a vast collection of videos. With its user-friendly interface and extensive library, Video Hub offers a comprehensive multimedia experience to users of all ages and interests.
+
+One of the main highlights of Video Hub is its diverse content library. It encompasses an array of video categories, including entertainment, sports, education, music, documentaries, and much more. Users can easily explore and discover videos that cater to their specific preferences, ensuring an engaging and personalized viewing experience.
+
+The website's intuitive search functionality allows users to find videos quickly and efficiently. Whether they are looking for specific titles, genres, or keywords, Video Hub's robust search feature helps users navigate through the vast collection with ease. Additionally, the platform provides curated recommendations based on users' viewing history, ensuring they discover new and exciting content aligned with their interests.
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+const MyCarousel = () => (
+  <Carousel
+    autoPlay
+    infiniteLoop
+    interval={1000}
+    showStatus={false}
+    showThumbs={false}
+    showArrows={false}
+  >
+    <Box w="full" h={'100vh'}>
+      <Image src={img1} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+        Watch The Future
+      </Heading>
+    </Box>
+    <Box w="full" h={'100vh'}>
+      <Image src={img2} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+        Future is Gaming
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img3} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Gaming on Console
+      </Heading>
+    </Box>
+
+    <Box w="full" h={'100vh'}>
+      <Image src={img4} h="full" w={'full'} objectFit={'cover'} />
+      <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+        Night life is cool
+      </Heading>
+    </Box>
+  </Carousel>
+);
+
+export default Home;
